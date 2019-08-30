9ca9318e4b447e4e9bf487c5f97871f94d762c25
AvilaMarco <marco6267@hotmail.com>
2019-08-30 14:29:57 -0300

    vue paginas estadisticas

--- js/estadisticas-web.js
+++ js/estadisticas-web.js
@@ -4,10 +4,11 @@ var estadisticas =
 	"Number_of_Democrats" : 0,
 	"Number_of_Republican" : 0,
 	"Number_of_Independents" : 0,
-	"votes_with_party_pct_Democrats" : 0,
-	"votes_with_party_pct_Republican" : 0,
-	"votes_with_party_pct_Independents" : 0,
-    "total_pct" :0
+	"votes_pct_Democrats" : 0,
+	"votes_pct_Republican" : 0,
+	"votes_pct_Independents" : 0,
+    "total_pct" :0,
+    "total_menbers": 0
 }
 
 // objetos html
@@ -17,14 +18,125 @@ let tableMost = document.querySelector("#Most-table");
 let attendace = document.querySelector("#attendance");
 let lolayty = document.querySelector("#lolayty");
 
-// datos json
-let datosCongress = datos.results[0].members;
+/*// datos json
+let datosCongress = datos.results[0].members;*/
 // variables javascript
 let congressRepublican = [];
 let congressDemocrats = [];
 let congressIndependents = [];
 
-listasPorPartido(datosCongress,congressRepublican,congressDemocrats,congressIndependents);
+traerJson("senate")
+
+function traerJson(pagina)
+{
+	fetch('https://api.propublica.org/congress/v1/113/'+pagina+'/members.json',{
+	method: 'GET',
+	headers :{'X-API-Key': 'Sn4XoZPWj9ebHDI5w77k3Jhb00q4kkIGfMxdc3Vr'}
+	}).then(function(response){if(response.ok){return response.json()}
+	}).then(function(data){
+    app.datosCongress = data.results[0].members;
+    let datosCongress = data.results[0].members;
+    listasPorPartido(datosCongress);
+      
+    //contruir tablas con las estadisticas
+    /*senateAtAGlance(tablaGlance);*/
+    if(attendace == null)
+    {
+        tableLoyal(false,"min");
+        tableLoyal(true,"max");
+        console.log("nice")
+    }else if(lolayty == null)
+    {
+        tableEngaged(app.menbersL,"min");
+        tableEngaged(app.menbersM,"max");
+    }
+
+
+    });
+}
+
+let app = new Vue({
+  el:'#app',
+  data:{
+    datosCongress:[],
+    name:"hola",
+    estadisticasVue:estadisticas,
+    menbersL:[],
+    menbersM:[]
+  }
+})
+
+
+
+function listasPorPartido(arrayMenber)
+{
+  let contR=0,contD=0,contI=0;
+  let pctR=0,pctD=0,pctI=0;
+  
+  arrayMenber.forEach(e=> {
+    switch(e.party){
+      case "R":
+        pctR+=e.votes_with_party_pct;
+        contR++;
+      break;
+      case "D":
+        pctD+=e.votes_with_party_pct;
+        contD++;
+      break;
+      case "I":
+        pctI+=e.votes_with_party_pct;
+        contI++;
+      break;
+  }
+  })
+  
+  estadisticas.total_pct = ((pctD+pctI+pctR)/arrayMenber.length).toFixed(2);
+  estadisticas.votes_pct_Democrats = (pctD/contD).toFixed(2);
+  estadisticas.votes_pct_Independents = contI==0?"-":(pctI/contI).toFixed(2);
+  estadisticas.votes_pct_Republican = (pctR/contR).toFixed(2);
+  estadisticas.total_menbers = arrayMenber.length;
+  estadisticas.Number_of_Democrats = contD;
+  estadisticas.Number_of_Independents = contI;
+  estadisticas.Number_of_Republican = contR;
+}
+
+function tableLoyal(isAscendente,orden)
+{
+  if(isAscendente){
+    app.menbersM = diesPorcientoBT("votes_with_party_pct",orden);
+  }else{
+    app.menbersL = diesPorcientoBT("votes_with_party_pct",orden);
+  }
+  /*objetoHtml.innerHTML = "";
+  for(let i = 0; i < datos.length;i++)
+    {
+      objetoHtml.innerHTML += `<tr>
+                    <td>${datos[i].first_name} ${(datos[i].middle_name || "")} ${datos[i].last_name}</td>
+                    <td>${Math.round(((datos[i].total_votes * datos[i].votes_with_party_pct)/100))}</td>
+                    <td>${datos[i].votes_with_party_pct.toFixed(2)}</td>
+                  </tr>`
+    }*/
+}
+
+function tableEngaged(isAscendente,orden)
+{
+  if(isAscendente){
+    app.menbersM = diesPorcientoBT("missed_votes_pct",orden);
+  }else{
+    app.menbersL = diesPorcientoBT("missed_votes_pct",orden);
+  }
+  /*objetoHtml.innerHTML = "";
+  for(let i = 0; i < datos.length;i++)
+    {
+      objetoHtml.innerHTML += `<tr>
+                    <td>${datos[i].first_name} ${(datos[i].middle_name || "")} ${datos[i].last_name}</td>
+                    <td>${datos[i].missed_votes}</td>
+                    <td>${datos[i].missed_votes_pct.toFixed(2)}</td>
+                  </tr>`
+    }*/
+}
+
+/*listasPorPartido(datosCongress,congressRepublican,congressDemocrats,congressIndependents);
 
 numeroRepresentantes(congressRepublican,congressDemocrats,congressIndependents);
 
@@ -40,63 +152,45 @@ if(attendace == null)
 {
 	tableEngaged(tableLeast,"min");
 	tableEngaged(tableMost,"max");
-}
-
-function listasPorPartido(datosMenber,Republicanx,Democratsx,Independentsx)
-{
-	for (var i = 0; i < datosMenber.length; i++)
-	{
-		switch(datosMenber[i].party)
-		{
-			case "R":
-				Republicanx.push(datosMenber[i]);
-			break;
-			case "D":
-				Democratsx.push(datosMenber[i]);
-			break;
-			case "I":
-				Independentsx.push(datosMenber[i]);
-			break;
-		}
-	}
-}
+}*/
 
 
-function numeroRepresentantes(Republicanx2,Democratsx2,Independentsx2)
-{
-	estadisticas.Number_of_Democrats = Democratsx2.length;
-	estadisticas.Number_of_Republican = Republicanx2.length;
-	estadisticas.Number_of_Independents = Independentsx2.length;
-}
 
-function porcentajeDeVotos(Republicanx2,Democratsx2,Independentsx2)
-{
-	let xRVote = 0,xDVote=0,xIVote=0;
-    let dividir = 3;
-	Republicanx2.forEach((menber) => xRVote += menber.votes_with_party_pct);
-	Democratsx2.forEach((menber) => xDVote += menber.votes_with_party_pct);
-	Independentsx2.forEach((menber) => xIVote += menber.votes_with_party_pct);
-    estadisticas.total_pct = (xRVote+xDVote+xIVote)/(Republicanx2.length+Democratsx2.length+Independentsx2.length);
-		xRVote = xRVote/Republicanx2.length;
-		estadisticas.votes_with_party_pct_Republican = xRVote;
-		xDVote = xDVote/Democratsx2.length;
-		estadisticas.votes_with_party_pct_Democrats = xDVote;
-		
-    if(Independentsx2.length!=0)xIVote = xIVote/Independentsx2.length;
-		estadisticas.votes_with_party_pct_Independents = xIVote;
-    
-}
+/*  function numeroRepresentantes(Republicanx2,Democratsx2,Independentsx2)
+  {
+      estadisticas.Number_of_Democrats = Democratsx2.length;
+      estadisticas.Number_of_Republican = Republicanx2.length;
+      estadisticas.Number_of_Independents = Independentsx2.length;
+  }
+
+  function porcentajeDeVotos(Republicanx2,Democratsx2,Independentsx2)
+  {
+      let xRVote = 0,xDVote=0,xIVote=0;
+      let dividir = 3;
+      Republicanx2.forEach((menber) => xRVote += menber.votes_with_party_pct);
+      Democratsx2.forEach((menber) => xDVote += menber.votes_with_party_pct);
+      Independentsx2.forEach((menber) => xIVote += menber.votes_with_party_pct);
+      estadisticas.total_pct = (xRVote+xDVote+xIVote)/(Republicanx2.length+Democratsx2.length+Independentsx2.length);
+          xRVote = xRVote/Republicanx2.length;
+          estadisticas.votes_with_party_pct_Republican = xRVote;
+          xDVote = xDVote/Democratsx2.length;
+          estadisticas.votes_with_party_pct_Democrats = xDVote;
+
+      if(Independentsx2.length!=0)xIVote = xIVote/Independentsx2.length;
+          estadisticas.votes_with_party_pct_Independents = xIVote;
+
+  }*/
 
 function diesPorcientoBT(propiedad,orden)
 {
-    let diezP = Math.floor((datosCongress.length * 10)/100);
+    let diezP = Math.floor((app.datosCongress.length * 10)/100);
     let minimosObjetos = [];
-		let minimosValores = obtenerArrayDatos(datosCongress,diezP,propiedad,orden);
+		let minimosValores = obtenerArrayDatos(app.datosCongress,diezP,propiedad,orden);
 		
     for(let i=0;i<minimosValores.length;i++)
 		{
 			let aux = minimosValores[i];
-			datosCongress.forEach((item) => item[propiedad] == aux ? minimosObjetos.push(item):null)
+			app.datosCongress.forEach((item) => item[propiedad] == aux ? minimosObjetos.push(item):null)
 		}
 	return minimosObjetos;
 }
@@ -132,7 +226,7 @@ function obtenerArrayDatos(Array,porcentaje,propiedad,orden)
 
 
 
-function senateAtAGlance(objetoHtml)
+/*function senateAtAGlance(objetoHtml)
 {
 	objetoHtml.innerHTMl ="";
 	objetoHtml.innerHTML =	`<tr>
@@ -155,32 +249,5 @@ function senateAtAGlance(objetoHtml)
                                 <td>${datosCongress.length}</td>
                                 <td>${estadisticas.total_pct.toFixed(2)}</td>
 							</tr>`
-}
+}*/
 
-function tableLoyal(objetoHtml,orden)
-{
-	let datos = diesPorcientoBT("votes_with_party_pct",orden);
-	objetoHtml.innerHTML = "";
-	for(let i = 0; i < datos.length;i++)
-		{
-			objetoHtml.innerHTML += `<tr>
-										<td>${datos[i].first_name} ${(datos[i].middle_name || "")} ${datos[i].last_name}</td>
-										<td>${Math.round(((datos[i].total_votes * datos[i].votes_with_party_pct)/100))}</td>
-										<td>${datos[i].votes_with_party_pct.toFixed(2)}</td>
-									</tr>`
-		}
-}
-
-function tableEngaged(objetoHtml,orden)
-{
-	let datos = diesPorcientoBT("missed_votes_pct",orden);
-	objetoHtml.innerHTML = "";
-	for(let i = 0; i < datos.length;i++)
-		{
-			objetoHtml.innerHTML += `<tr>
-										<td>${datos[i].first_name} ${(datos[i].middle_name || "")} ${datos[i].last_name}</td>
-										<td>${datos[i].missed_votes}</td>
-										<td>${datos[i].missed_votes_pct.toFixed(2)}</td>
-									</tr>`
-		}
-}
--- js/main.js
+++ js/main.js
@@ -2,18 +2,13 @@
 let tBodySenate = document.querySelector('#senate-data');
 let tBodyHouse = document.querySelector('#house-data');
 let carga = document.querySelector('#carga');
-/*let checkbox = document.querySelectorAll('form input');*/
-let selectState = document.querySelector('#state-filter');
-
-// datos json
-let datosCongress = datos.results[0].members;
 
 // variables javascript
 let primeraCarga = true;
 let cargaActualHtml;
 let animationCarga;
-let states=[];
-let state;
+
+definirCarga()
 
 let app = new Vue({
     el:'#app',
@@ -21,56 +16,99 @@ let app = new Vue({
         
         checkbox:[
             {
-                check:false,
                 value:"R",
                 name:"Republican"
             },
             {
-                check:false,
                 value:"D",
                 name:"Democrat"
             },
             {
-                check:false,
                 value:"I",
                 name:"Independent"
             }
         ],
-        datosCongress:datosCongress,
+        primeraCarga:true,
+        datosCongress:[],
         selecStates:[],
         valueSelect:"all",
-        tabla:[]
+        tablaux:[],
+        checkboxaux:[]
     },
     methods:{
-        valorBox(){
-                this.checkbox.filter(e=>e.check)
-            },
         selects(){
-            datosCongress.forEach(e=>this.selecStates.indexOf(e.state)==-1?this.selecStates.push(e.state):null)
+        this.datosCongress.forEach(e=>this.selecStates.indexOf(e.state)==-1?this.selecStates.push(e.state):null)
         },
         filter(){
-        if(true){
-            this.tabla = []
-            this.datosCongress=datosCongress
-            this.datosCongress.forEach(e=>this.valueSelect==e.state?this.tabla.push(e):null)
-            this.datosCongress = this.tabla
-        }
+          this.primeraCarga=false
+          this.tablaux = []
+          let filtrado = (this.valueSelect!="all" && this.checkboxaux.length!=0)
+          this.datosCongress.forEach(e=>((filtrado) ? (this.checkboxaux.indexOf(e.party)>=0 && this.valueSelect==e.state):(this.checkboxaux.indexOf(e.party)>=0 || this.valueSelect==e.state))?this.tablaux.push(e):null)
+          if(this.valueSelect=="all" && this.checkboxaux.length==0){this.primeraCarga=true}
         }
     }
 })
 
-app.selects()
+function traerJson(pagina)
+{
+	fetch('https://api.propublica.org/congress/v1/113/'+pagina+'/members.json',
+    {
+	method: 'GET',
+	headers :{'X-API-Key': 'Sn4XoZPWj9ebHDI5w77k3Jhb00q4kkIGfMxdc3Vr'}
+	}).then(function(response){ if(response.ok){return response.json() }
+	}).then(function(data){
+	app.datosCongress = data.results[0].members;
+    app.selects()
+    if (animationCarga){carga.style.display="none";animationCarga = false;}
+	});
+}
 
+function definirCarga()
+{
+	if(tBodyHouse==null)
+	{
+		animationCarga = false;
+		cargaActualHtml = tBodySenate;
+        traerJson("senate")
+	}else if(tBodySenate==null){
+		animationCarga = true;
+		cargaActualHtml = tBodyHouse;
+        traerJson("house")
+	}
+}
+
+/*
+let filtrado = (state!="all" && filter.length!=0)
+let filtroEstado = datosMenber[i].state == state;
+		let filtroPartido = filter.indexOf(datosMenber[i].party) >= 0;
+		// variable para decidir si es filtrado simple o combinado
+		let filtradoActual = (filtrado ? (filtroPartido && filtroEstado) : (filtroPartido || filtroEstado))
+
+		if (primeraCarga || filtradoActual || (state =="all" && filter.length==0))
+		{*/
+    /*this.valueSelect==e.state*/
+
+/*this.checkboxaux.length!=0&&this.valueSelect!="all"?(this.checkboxaux.indexOf(e.party)!=-1&&this.valueSelect==e.state):(this.checkboxaux.indexOf(e.party)!=-1||this.valueSelect==e.state)*/
+
+/*let checkbox = document.querySelectorAll('form input');*/
+    /*let selectState = document.querySelector('#state-filter');*/
+
+/*// datos json
+let datosCongress = datos.results[0].members;*/
+
+/*let states=[];
+let state;*/
 // elije la pagina en la que agrega los datos de la tabla
-definirCarga();
+/*definirCarga();*/
 // agrega las option al select
 /*buildSelect(datosCongress,states,selectState);*/
 // construye la tabla por defecto, sin filtros
 /*buildTable(datosCongress,cargaActualHtml,checkbox);*/
 // agrega el escuchador de eventos a los inputs checkbox y al select
 /*checkbox.forEach(box => box.addEventListener("change",filter))*/
-selectState.addEventListener("change",filter);
+/*selectState.addEventListener("change",filter);*/
 
+/*
 function buildSelect(datosMenber,arrayState,elementHTML)
 {
 	for (let i = 0; i < datosMenber.length; i++)
@@ -89,19 +127,9 @@ function filter(target)
 	buildTable(datosCongress,cargaActualHtml,checkbox,state);
 }
 
-function definirCarga()
-{
-	if(tBodyHouse==null)
-	{
-		cargaActualHtml = tBodySenate;
-		animationCarga = false;
-	}else if(tBodySenate==null){
-		cargaActualHtml = tBodyHouse;
-		animationCarga = true;
-	}
-}
+*/
 
-function buildTable(datosMenber,elementHTML,tilde,state)
+/*function buildTable(datosMenber,elementHTML,tilde,state)
 {
 	// // vacia la tabla
 	elementHTML.innerHTML = "";
@@ -134,4 +162,4 @@ function buildTable(datosMenber,elementHTML,tilde,state)
 	}
 	if (animationCarga)carga.style.display="none";
 	primeraCarga = false;
-}
+}*/
